210b62ee26119f49fce9cc9d78798c2c2638eafa
Lewin Chan <lewin.chan@adaptris.com>
2016-11-09 13:52:12 +0000

    Made public due to the possibility of additional document builders in other packages.

--- src/main/java/com/adaptris/core/elastic/DocumentWrapper.java
+++ src/main/java/com/adaptris/core/elastic/DocumentWrapper.java
@@ -2,7 +2,7 @@ package com.adaptris.core.elastic;
 
 import org.elasticsearch.common.xcontent.XContentBuilder;
 
-class DocumentWrapper {
+public class DocumentWrapper {
 
   private final String uniqueId;
   private final XContentBuilder content;
@@ -12,11 +12,11 @@ class DocumentWrapper {
     this.content = content;
   }
   
-  XContentBuilder content() {
+  public XContentBuilder content() {
     return content;
   }
 
-  String uniqueId() {
+  public String uniqueId() {
     return uniqueId;
   }
 
